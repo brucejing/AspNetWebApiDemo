5d54b1d0163be6ecb354b29b8468693692e95466
Bruce <codewithbruce@hotmail.com>
2025-05-07 16:14:21 -0400

    .NET 8 Web API default template code uses Swagger to generate API schema and Swagger UI to present API document.

--- DotNet8Api/Program.cs
+++ DotNet8Api/Program.cs
@@ -11,16 +11,16 @@ namespace DotNet8Api
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+            // builder.Services.AddEndpointsApiExplorer(); // required only for minimal APIs
+            builder.Services.AddSwaggerGen(); // Add the Swagger generator to the services collection
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
             if (app.Environment.IsDevelopment())
             {
-                app.UseSwagger();
-                app.UseSwaggerUI();
+                app.UseSwagger(); // Enable the middleware for serving the generated JSON document at /swagger/v1/swagger.json
+                app.UseSwaggerUI(); // Enable the middleware for an embedded version of the Swagger UI tool at /swagger
             }
 
             app.UseAuthorization();
