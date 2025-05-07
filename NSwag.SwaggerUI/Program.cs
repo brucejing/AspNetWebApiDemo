c57547f910c4d96a873a0c4a7f42380fc7f5fb5e
Bruce <codewithbruce@hotmail.com>
2025-05-07 17:03:34 -0400

    Add NSwag to generate API specification and use Swagger UI to show API document.

--- NSwag.SwaggerUI/Program.cs
+++ NSwag.SwaggerUI/Program.cs
@@ -9,10 +9,21 @@ namespace NSwag.SwaggerUI
             // Add services to the container.
 
             builder.Services.AddControllers();
+            builder.Services.AddOpenApiDocument();
 
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            if (app.Environment.IsDevelopment())
+            {
+                // Add OpenAPI 3.0 document serving middleware
+                // Available at: http://localhost:<port>/swagger/v1/swagger.json
+                app.UseOpenApi();
+
+                // Add web UIs to interact with the document
+                // Available at: http://localhost:<port>/swagger
+                app.UseSwaggerUi(); // UseSwaggerUI Protected by if (env.IsDevelopment())
+            }
 
             app.UseAuthorization();
 
